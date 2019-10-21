2f99143e4ce343a40e7f04a00d44aa51307460fb
Tyler <tyler.buell@sparkbusinessworks.com>
2019-10-20 21:33:12 -0400

    removing Blog Code Files

--- feedback.php
+++
@@ -1,9 +0,0 @@
-<?php if (count($feedback) > 0): ?>
-	<div class="feedback">
-		<?php foreach ($feedback as $message): ?>
-			<p style = "color: white;">
-				<?php echo $message ?>	
-			</p>
-		<?php endforeach ?>		
-	</div>
-<?php endif ?>
